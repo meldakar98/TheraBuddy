69118775d364599b47a086c17f7f93cab66a1e5b
eldak <eldakar@students.uni-marburg.de>
2024-02-07 21:26:17 +0100

    please work

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -7,7 +7,7 @@ import MessageList from '../MessageList/MessageList';
 import ChatInput from '../ChatInput/ChatInput';
 import OpenAI from 'openai';
 function Chat(props) {
-  const { dataSource, triggerScrollToBottom, setTriggerScrollToBottom, activeId, setActive, setChanges } = props;
+  const { added,setAdded,dataSource, triggerScrollToBottom, setTriggerScrollToBottom, activeId, setActive, setChanges } = props;
 
   const MAX_NUM_MESSAGES = 50;
   const [messagesModel, setMessagesModel] = useState(dataSource);
@@ -38,7 +38,7 @@ function Chat(props) {
   const openai = new OpenAI({ apiKey: "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", dangerouslyAllowBrowser: true });
   const submitMessage = async (message) => {
     //here generate response
-    console.log("nsnasnn" + message)
+  console.log("nsnasnn" + message)
     let msg = "";
     let msgs = [...apiMessages];
     if (msgs.length > 5) {
@@ -87,7 +87,6 @@ function Chat(props) {
     if (messageToSend) {
 
       submitMessage(messageToSend.message);
-      console.log("hey" + messageToSend.message);
 
     }
 
@@ -126,7 +125,7 @@ function Chat(props) {
 
   const [show, setShow] = useState(false);
 
-  const handleClose = () => { setShow(false); setApiMessages([systemJson]); setUpdated(true); }
+  const handleClose = () => { setShow(false); setApiMessages([systemJson]); setUpdated(true) ;}
   const handleShow = () => setShow(true);
 
 
@@ -140,7 +139,9 @@ function Chat(props) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(chat)
       }).then(() => {
+        
         console.log('new chat added ');
+        setAdded(added+1);
       });
     }
     else {
@@ -150,7 +151,7 @@ function Chat(props) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(message)
       }).then(() => {
-        console.log('new blog deleted');
+        console.log(' blog deleted');
       });
       const chat = { date, apiMessages };
       fetch('http://localhost:8000/chats/', {
@@ -159,6 +160,8 @@ function Chat(props) {
         body: JSON.stringify(chat)
       }).then(() => {
         console.log('new chat added ');
+        
+        setAdded(added+1);
       });
     }
     handleClose();
